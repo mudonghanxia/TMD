a3ba4d2355ab92fb58b7ac828bbd643339a80a13
cold_summer <66187119+mudonghanxia@users.noreply.github.com>
2021-05-22 10:51:48 +0800

    Update 跳转语句.js

--- javascript/跳转语句.js
+++ javascript/跳转语句.js
@@ -1,6 +1,6 @@
 function fun(){
     loop: for( let j = 1;j < 6; j++){
-        document.write("<br>" + j + ":")l
+        document.write("<br>" + j + ":")
         for(let i = 1; i < 6; i++){
             if(i == 3) continue loop;
             document.write(i);
